8b4f45888b7b8932e9d58b7d78b45580db8a20d6
trueJinias <sanada.tomoya57@gmail.com>
2026-02-07 19:54:54 +0900

    Add output path to vite config and GitHub Action for theme deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './i18n'; // Import i18n configuration
 
+console.log('VogueLITZ: React App Initializing...');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
