544b0bc5d37ceabb12a585f3eec093f52f1fb152
trueJinias <sanada.tomoya57@gmail.com>
2026-02-07 20:04:02 +0900

    Fix React ReferenceError by injecting React import globally

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   build: {
     outDir: 'shopify_theme/assets',
     emptyOutDir: true,
